24d274f02f5a0a3c1fab88d51de50f49020fdbdd
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-09-11 22:29:26 -0600

    added e2e test for register

--- e2e/register.test.js
+++ e2e/register.test.js
@@ -1,6 +1,9 @@
 import {Selector} from 'testcafe';
 
 const TEST_URL = process.env.TEST_URL;
+const randomstring = require('randomstring');
+const username = randomstring.generate();
+const email = `${username}@test.com`;
 
 fixture('/register').page(`${TEST_URL}/register`);
 
@@ -9,4 +12,23 @@ test(`should display the registration form`, async(t) => {
         .navigateTo(`${TEST_URL}/register`)
         .expect(Selector('H1').withText('Register').exists).ok()
         .expect(Selector('form').exists).ok()
+});
+
+test(`should allow a user to register`, async(t)=>{
+    await t
+        .navigateTo(`${TEST_URL}/register`)
+        .typeText('input[name="username"]', username)
+        .typeText('input[name="email"]', email)
+        .typeText('input[name="password"]', 'password123')
+        .click(Selector('input[type="submit"]'))
+
+    const tableRow = Selector('td').withText(username).parent();
+    await t
+        .expect(Selector('H1').withText('All Users').exists).ok()
+        .expect(tableRow.child().withText(username).exists).ok()
+        .expect(tableRow.child().withText(email).exists).ok()
+        .expect(Selector('a').withText('User Status').exists).ok()
+        .expect(Selector('a').withText('Log Out').exists).ok()
+        .expect(Selector('a').withText('Register').exists).notOk()
+        .expect(Selector('a').withText('Log In').exists).notOk()
 });
