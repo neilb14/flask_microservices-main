fd6f410e98e39800af5598a96cdee4fc5df6c23b
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-09-18 07:45:40 -0600

    testing flash message on login

--- e2e/login.test.js
+++ e2e/login.test.js
@@ -84,6 +84,7 @@ test(`should allow a user to sign in`, async (t) => {
         .expect(Selector('a').withText('Log Out').exists).ok()
         .expect(Selector('a').withText('Register').exists).notOk()
         .expect(Selector('a').withText('Log In').exists).notOk()
+        .expect(Selector('.alert-success').withText('Welcome!').exists).ok()
     
       // log a user out
       await t
